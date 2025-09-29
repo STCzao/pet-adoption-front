ffacee53089567af6dcffe8f98499a51bfc9b827
STCzao <Santicoronel0183@gmail.com>
2025-09-29 19:44:51 -0300

    Agregamos los componentes de ForgotPassword y ResetPassword para la recuperacion de contraseña

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,17 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "../src/pages/LoginScreen/LoginScreen";
 import RegisterScreen from "../src/pages/RegisterScreen/RegisterScreen";
+import ForgotPasswordScreen from "../src/pages/ForgotPasswordScreen/ForgotPasswordScreen";
+import ResetPasswordScreen from "../src/pages/ResetPasswordScreen/ResetPasswordScreen";
 import ProtectedRoutes from "../src/routes/ProtectedRoutes/ProtectedRoutes";
 import HomeScreen from "../src/pages/HomeScreen/HomeScreen";
 
 function App() {
-  // Estados para manejar login y datos de usuario
   const [login, setLogin] = useState(false);
   const [user, setUser] = useState(null);
 
-  // Función para guardar datos del usuario autenticado
-  const guardarUsuario = (datos) => {
-    setUser(datos);
-  };
-
-  // Función cuando inicia sesión
-  const iniciarSesion = () => {
-    setLogin(true);
-  };
-
-  // Función cuando cierra sesión
+  const guardarUsuario = (datos) => setUser(datos);
+  const iniciarSesion = () => setLogin(true);
   const cerrarSesion = () => {
     setLogin(false);
     setUser(null);
@@ -30,7 +22,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Ruta Login */}
+        {/* Login */}
         <Route
           path="/login"
           element={
@@ -41,8 +33,15 @@ function App() {
           }
         />
 
-        {/* Ruta Register */}
+        {/* Register */}
         <Route path="/register" element={<RegisterScreen />} />
+
+        {/* Recuperar contraseña */}
+        <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
+        <Route
+          path="/reset-password/:token"
+          element={<ResetPasswordScreen />}
+        />
 
         {/* Rutas protegidas */}
         <Route
