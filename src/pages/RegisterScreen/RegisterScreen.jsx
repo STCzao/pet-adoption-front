df77882deff303673bdf39e74efbd281e7e40c73
STCzao <Santicoronel0183@gmail.com>
2025-10-06 19:44:30 -0300

    Empezamos a realizar cambios esteticos

--- src/pages/RegisterScreen/RegisterScreen.jsx
+++ src/pages/RegisterScreen/RegisterScreen.jsx
@@ -211,7 +211,7 @@ export default function RegisterScreen() {
 
           <button
             type="submit"
-            className="mt-6 w-full h-11 rounded-full text-white bg-black border border-white/70 hover:bg-[#FF7857] transition-opacity"
+            className="mt-6 w-full h-11 rounded-full text-white bg-white/20 border border-white/70 hover:bg-[#FF7857] transition-opacity"
           >
             Registrarse
           </button>
--- src/pages/ResetPasswordScreen/ResetPasswordScreen.jsx
+++ src/pages/ResetPasswordScreen/ResetPasswordScreen.jsx
@@ -134,7 +134,7 @@ const ResetPasswordScreen = () => {
 
           <button
             type="submit"
-            className="mt-6 w-full h-11 rounded-full text-white bg-black border border-white/70 hover:bg-[#FF7857] transition-opacity"
+            className="mt-6 w-full h-11 rounded-full text-white bg-white/20 border border-white/70 hover:bg-[#FF7857] transition-opacity"
           >
             Actualizar contraseña
           </button>
