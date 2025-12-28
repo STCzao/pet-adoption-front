f01561c7fe633bd84b1f2fc4d9dae841a68fabdb
STCzao <Santicoronel0183@gmail.com>
2025-12-28 19:46:54 -0300

    Cambios en el texto de que hacer en caso de adopciones, en el titulo del filtro y cambios la redireccion de los links del footer

--- src/components/CardFiltro/CardFiltro.jsx
+++ src/components/CardFiltro/CardFiltro.jsx
@@ -6,7 +6,7 @@ const CardFiltro = ({ filtros, setFiltros }) => {
 
   return (
     <div className="bg-[#763A0D]/70 backdrop-blur max-w-[200px] border border-white/20 rounded-xl shadow-md p-2 text-white font-medium flex flex-col">
-      <h3 className="text-lg font-bold mb-2">Filtros</h3>
+      <h3 className="text-lg font-bold mb-2">Buscar por...</h3>
 
       {/* Raza (texto libre) */}
       <div>
